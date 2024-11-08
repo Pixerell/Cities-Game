3fea276170230c94470af6b8e9cdd7dbda6d53e6
Pixerell <alexmegaflexer@gmail.com>
2024-11-08 15:41:27 +0300

    Task 1 - Initial bot

---
+++ cities.py
@@ -0,0 +1,22 @@
+import telebot
+TOKEN = '7827971601:AAF9prZTfj609NI2PJUBOXtLgihUUK0w6AI'
+
+
+bot=telebot.TeleBot(TOKEN)
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "GAME START!")
+
+
+@bot.message_handler(commands=['stop'])
+def stop(message):
+    bot.send_message(message.chat.id, "GAME DEAD!")
+
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.send_message(message.chat.id,
+    "List of following commands:\n/start - start the game \n /stop - stop the game \n/help - list of commands (and help)")
+
+
+bot.polling(none_stop=True, interval=0)
